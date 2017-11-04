92913d7243578e6825443729e513dff2158ee7bf
Rogalik <rogalski92@hotmail.com>
2017-11-04 21:43:29 +0100

    6.5 klasa StatisticCount

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCount.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCount.java
@@ -4,6 +4,10 @@ public class StatisticsCount {
 
     public double calculateAdvStatistics(Statistics statistics) {
 
+        int userQuantity = statistics.userNames().size();
+        int postsQuantity = statistics.postsCount();
+        int commentQuantity = statistics.commentsCount();
+
         double averageOne = statistics.postsCount() / statistics.userNames().size();
         double averageTwo = statistics.commentsCount() / statistics.userNames().size();
         double averageThree = statistics.commentsCount() / statistics.postsCount();

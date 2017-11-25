9cc1be3a8f09dca2ecc49812d88288ca4ebe1bf0
Rogalik <rogalski92@hotmail.com>
2017-11-25 17:46:21 +0100

    Spring

---
+++ kodilla-spring/src/test/java/com/kodilla/kodillaspring/SpringRunnerTests.java
@@ -0,0 +1,21 @@
+//package com.kodilla.kodillaspring;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class SpringRunnerTests {
+//
+//	@Test
+//	public void testCircleAddedToTheContainer() {
+//
+//		ApplicationContext context =
+//				new AnnotationConfigApplicationContext("com.")
+//	}
+//
+//}

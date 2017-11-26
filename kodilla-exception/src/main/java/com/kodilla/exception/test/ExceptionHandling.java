3d82463e1e742f290ca689b1958f807415364119
Rogalik <rogalski92@hotmail.com>
2017-11-26 17:00:31 +0100

    8.3 Throws

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,14 +1,16 @@
 package com.kodilla.exception.test;
 
-import java.io.File;
-
 public class ExceptionHandling {
 
-    public void readFile(){
-
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("SecondChallenge/probablyWillThrowException").getFile());
-
-        System.out.println(file.getPath());
+    public static void main(String [] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            secondChallenge.probablyWillThrowException(1,1.5);
+        } catch (Exception e) {
+            System.out.println("Manual exception" + " " + e);
+        }
+        finally {
+            System.out.println("This is going to be here always");
+        }
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
@@ -1,10 +1,11 @@
-//package com.kodilla.com.kodilla.exception.test;
-//
-//public class SecondChallenge {
-//    public String probablyWillThrowException(double x, double y){
-//        if(x >= 2 || x < 1 || y == 1.5) {
-//            throw new Exception();
-//        }
-//        return "Done!";
-//    }
-//}
+package com.kodilla.exception.test;
+
+public class SecondChallenge {
+    public String probablyWillThrowException(double x, double y) throws Exception{
+        if(x >= 2 || x < 1 || y == 1.5) {
+            throw new Exception();
+        }
+        return "Done!";
+    }
+}
+

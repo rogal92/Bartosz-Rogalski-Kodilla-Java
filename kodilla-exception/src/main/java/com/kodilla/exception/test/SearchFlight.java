174ccb402fd2525db2c01a8d3706278eac44a552
Rogalik <rogalski92@hotmail.com>
2017-11-27 17:49:03 +0100

    8.4 wiedza wyjątkach

--- kodilla-exception/src/main/java/com/kodilla/exception/test/SearchFlight.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SearchFlight.java
@@ -4,17 +4,35 @@ import java.util.HashMap;
 
 public class SearchFlight {
 
+    HashMap<String, Boolean> airportsAvailibility = new HashMap<>();
+
+    public SearchFlight(HashMap<String, Boolean> airportsAvailibility) {
+        this.airportsAvailibility = airportsAvailibility;
+
+        airportsAvailibility.put("AirportOne",true);
+        airportsAvailibility.put("AirportTwo", false);
+        airportsAvailibility.put("AirportThree", true);
+        airportsAvailibility.put("AirportFour", false);
+        airportsAvailibility.put("AirportFive", true);
+        airportsAvailibility.put("AirportSix", true);
+    }
+
     public void findFlight(Flight flight) throws RouteNotFoundException {
 
-        HashMap<String, Boolean> AirportsAvailibility = new HashMap<>();
+        Boolean contains = airportsAvailibility.containsKey("AirportSeven");
 
-        AirportsAvailibility.put("AirportOne", true);
-        AirportsAvailibility.put("AirportTwo", false);
-        AirportsAvailibility.put("AirportThree", true);
-        AirportsAvailibility.put("AirportFour", false);
-        AirportsAvailibility.put("AirportFive", true);
-        AirportsAvailibility.put("AirportSix", true);
+        if (contains == false) {
+            throw new RouteNotFoundException();
+        }
+    }
+    public static void main(String[] args) {
 
+        try {
 
+        } catch (Exception e) {
+            System.out.println("This airport is not available");
+        } finally {
+            System.out.println("Thank you for using our airlines");
+        }
     }
 }

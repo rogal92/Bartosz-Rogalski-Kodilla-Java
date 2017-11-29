fd267dc80093f37456e75fb3a959a0795d4eba76
Rogalik <rogalski92@hotmail.com>
2017-11-29 22:35:02 +0100

    Spring

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -1,4 +1,22 @@
 package com.kodilla.spring.forum;
 
-public class ForumTestSuite {
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ForumUserTestSuite {
+
+    @Test
+    public void testGetUsername() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.forum");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+
+        //When
+        String name = forumUser.getUsername();
+
+        //Then
+        Assert.assertEquals("John Smith", name);
+    }
 }

b2376d9133bf927549350ad8613d8ac08fc963b3
Rogalik <rogalski92@hotmail.com>
2017-11-30 12:40:25 +0100

    10.2

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Test;
+
+public class CalculatorTestSuite {
+
+    @Test
+    public void testCalculations(){
+        //Given
+        //When
+        //Then
+    }
+}

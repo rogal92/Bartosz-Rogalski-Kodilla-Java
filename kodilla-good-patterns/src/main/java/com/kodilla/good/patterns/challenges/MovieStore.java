6336ec4195c22587a4e714eb5026b153b92a6e7f
Rogalik <rogalski92@hotmail.com>
2017-12-01 19:11:46 +0100

    9.1 idea czystego kodu

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -25,9 +25,99 @@ public class MovieStore {
         booksTitlesWithTranslations.put("AV", avengersTranslations);
         booksTitlesWithTranslations.put("FL", flashTranslations);
 
-        return booksTitlesWithTranslations;
+        return booksTitlesWithTranslations.stream()
+                .
     }
 }
 //        Wykorzystując poznane już Streamy wykonaj iterację po poniższej mapie tytułów i wyświetl wszystkie tytuły w jednym ciągu separując
 //        je wykrzyknikiem:
 
+//package com.kodilla.stream.forum;
+//
+//        import java.util.HashSet;
+//        import java.util.Set;
+//        import java.util.stream.Collectors;
+//
+//public final class ForumUser {
+//
+//    private final String userName;
+//    private final String realName;
+//    private final String location;
+//    private final Set<ForumUser> friends = new HashSet<>();
+//
+//    public ForumUser(String userName, String realName, String location) {
+//        this.userName = userName;
+//        this.realName = realName;
+//        this.location = location;
+//    }
+//
+//    public void addFriend(ForumUser user){
+//        friends.add(user);
+//    }
+//
+//    public boolean removeFriend(ForumUser user) {
+//        return friends.remove(user);
+//    }
+//    public Set<String> getLocationsOffFriends(){
+//        return friends.stream()
+//                .map(friend -> friend.getLocation())
+//                .collect(Collectors.toSet());
+//    }
+//    public Set<String> getLocationsOfFriendsOfFriends(){
+//        return friends.stream()
+//                .flatMap(user -> user.getFriends().stream())
+//                .filter(user -> user != this)
+//                .map(ForumUser::getLocation)
+//                .collect(Collectors.toSet());
+//    }
+//
+//    public String getUserName() {
+//        return userName;
+//    }
+//
+//    public String getRealName() {
+//        return realName;
+//    }
+//
+//    public String getLocation() {
+//        return location;
+//    }
+//
+//    public Set<ForumUser> getFriends() {
+//        return friends;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        ForumUser forumUser = (ForumUser) o;
+//
+//        return userName.equals(forumUser.userName);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return userName.hashCode();
+//    }
+//}
+//
+//public class SimpleInvoice {
+//    private final List<SimpleItem> items = new ArrayList<>();
+//
+//    public void addItem(SimpleItem item) {
+//        items.add(item);
+//    }
+//
+//    public boolean removeItem(SimpleItem item) {
+//        return items.remove(item);
+//    }
+//
+//    public double getValueToPay() {
+//        return items.stream()
+//                .collect(Collectors.summingDouble(SimpleItem::getValue));
+//
+//    }
+//}
+//

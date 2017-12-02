18991b2e150a209ffb8aee77f0ce73c6b723c7bc
Rogalik <rogalski92@hotmail.com>
2017-12-02 14:03:12 +0100

    9.2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/InformationServiceImpl.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/InformationServiceImpl.java
@@ -3,10 +3,8 @@ package com.kodilla.good.patterns.challenges;
 public class InformationServiceImpl implements InformationService {
     @Override
     public void inform(User user) {
-
-    }
-
-    public InformationServiceImpl() {
         System.out.println("I am here to inform the user about the order");
     }
+
+    public InformationServiceImpl() {}
 }

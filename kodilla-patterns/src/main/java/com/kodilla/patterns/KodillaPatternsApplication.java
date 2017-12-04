b109a2e2bd394f60e01b3e2fc37675b64001e180
Rogalik <rogalski92@hotmail.com>
2017-12-04 16:01:05 +0100

    11.1

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/KodillaPatternsApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaPatternsApplication.class, args);
+	}
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,17 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+
+    @Test
+    public void testGetLastLog() {
+        //Given
+        //When
+        String lastLog = Logger.getInstance().getLastLog();
+        System.out.println(lastLog);
+        //Then
+        Assert.assertEquals("",lastLog);
+    }
+}

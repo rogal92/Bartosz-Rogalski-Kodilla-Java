78abfe794ce34a4406aaabdf13443f2c8c2cc836
Rogalik <rogalski92@hotmail.com>
2017-12-07 18:40:40 +0100

    Bigmac

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .roll("Standard")
+                .burgers(3)
+                .sauce("Thousand Islands")
+                .ingredient("Cheese")
+                .ingredient("Becon")
+                .ingredient("Chilli")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int ingredientsAmount = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(3, ingredientsAmount);
+    }
+}

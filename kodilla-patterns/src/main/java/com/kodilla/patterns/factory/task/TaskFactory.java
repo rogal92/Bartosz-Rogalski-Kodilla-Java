c65f3734ce2b0bf24c1e2231ca72ceeda79b5d4f
Rogalik <rogalski92@hotmail.com>
2017-12-07 16:59:13 +0100

    11.4

--- kodilla-patterns/src/main/java/com/kodilla/patterns/factory/task/TaskFactory.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/task/TaskFactory.java
@@ -5,17 +5,16 @@ public class TaskFactory {
     public static final String PAINTINGTASK = "PAINTINGTASK";
     public static final String SHOPPINGTASK = "SHOPPINGTASK";
 
-    public final Task doTask(final String taskClass) {
+    public final Task doTask(final String taskClass) throws IllegalArgumentException {
         switch(taskClass) {
             case DRIVINGTASK:
                 return new DrivingTask("Make an eight","Near Driving School","Honda Africa Twin");
             case PAINTINGTASK:
                 return new PaintingTask("Test your skills","Black&White","Mona Lisa");
             case SHOPPINGTASK:
-                return new ShoppingTask("Spend 1 Million in 1 day","Only usefull thinks",1000000.0);
+                return new ShoppingTask("Spend 1 Million in 1 day","Only usefull things",1000000.0);
             default:
-                return null;
-
+                throw new IllegalArgumentException("Type the right task name");
         }
     }
 }

aa6fb4e9f20074cdf57fb23412d28f2cbf755554
Rogalik <rogalski92@hotmail.com>
2018-01-26 15:22:21 +0100

    20.3

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -2,6 +2,8 @@ package com.kodilla.patterns2.facade.api;
 
 import com.kodilla.patterns2.facade.Order;
 import com.kodilla.patterns2.facade.ShopService;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +11,22 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+@Aspect
 @Component
 public class  OrderFacade {
     @Autowired
     private ShopService shopService;
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
 
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+    "&& args(userId) && target(order)")
+
     public void processOrder(final OrderDto order, final Long userId)
         throws OrderProcessingException {
+
+        LOGGER.info("Order: " + order.getItems().iterator() + ", Args: " + userId);
+
         boolean wasError = false;
         long orderId = shopService.openOrder(userId);
         LOGGER.info("Registering new order, ID: " + orderId);
